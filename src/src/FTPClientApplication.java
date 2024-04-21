0cc7c29bc2501780dbebfe4cebe36952159b10bc
mateusVedoy <mateusvedoy@outlook.com>
2024-04-21 16:01:15 -0300

    refatora main para utilizar ações de cliente

--- src/src/FTPClientApplication.java
+++ src/src/FTPClientApplication.java
@@ -1,11 +1,18 @@
+import action.Authenticate;
+import action.ActiveMode;
+import action.UploadFile;
+
 import java.io.*;
 import java.net.Socket;
-import java.net.UnknownHostException;
 
 public class FTPClientApplication {
 
+    private static Authenticate auth;
+    private static ActiveMode activeMode;
+    private static UploadFile uploadFile;
+
     public static void main(String args[]) {
-        String server = "localhost";
+        String server = "127.0.0.1";
         int port = 1025;
         String username = "server";
         String password = "server";
@@ -16,50 +23,30 @@ public class FTPClientApplication {
              BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
              BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()))) {
 
+            //INSTANCES
+            auth = new Authenticate(reader, writer);
+            activeMode = new ActiveMode(reader, writer, socket);
+            uploadFile = new UploadFile(reader, writer, socket);
+
+            //SERVER WELCOME
             String response = reader.readLine();
-            System.out.println("Response: " + response);
+            System.out.println("Server Welcome" + response);
 
-            writer.write("USER " + username + "\r\n");
-            writer.flush();
-            response = reader.readLine();
-            System.out.println("Response: " + response);
+            auth.execute(username+","+password);
 
-            //ENVIA PORTA DO CLIENTE PARA GERAR CONEXÃO
-            int localPort = socket.getLocalPort();
-            String host = socket.getLocalAddress().getHostAddress();
-            System.out.println(localPort);
-            System.out.println(host);
-            int p1 = localPort / 256;
-            int p2 = localPort % 256;
-            writer.write("PORT 127,0,0,1,"+p1+","+p2+"\r\n");
-            writer.flush();
-            response = reader.readLine();
-            System.out.println("Response: " + response);
+            String address = server.replace(".", ",");
+            activeMode.execute(address);
 
+            //aqui depois vai ser passado pelo usuario
+            String pathToFile = "./files/origin.txt";
+            uploadFile.execute(pathToFile);
 
-            send(socket, writer, reader);
-            System.out.println(reader.readLine());
+            System.out.println("fim do programa");
 
-        } catch (IOException e) {
+        } catch (RuntimeException e) {
+            System.out.println(e.getMessage());
+        }catch (IOException e) {
             e.printStackTrace();
         }
-    }
-
-    private static void send(Socket socket, BufferedWriter writer, BufferedReader reader) throws IOException {
-        FileInputStream fis = new FileInputStream("./files/origin.txt");
-        BufferedInputStream bis = new BufferedInputStream(fis);
-        OutputStream outputStream = socket.getOutputStream();
-
-        writer.write("STOR server.txt\r\n");
-        writer.flush();
-
-        byte[] buffer = new byte[1024];
-        int bytesRead;
-        while ((bytesRead = bis.read(buffer)) != -1) {
-            outputStream.write(buffer, 0, bytesRead);
-        }
-
-        fis.close();
-        bis.close();
     }
 }
