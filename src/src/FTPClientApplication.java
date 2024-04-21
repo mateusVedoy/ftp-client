4f05a4003cf2a87ed94c14ac180be6242c04e5e6
mateusVedoy <mateusvedoy@outlook.com>
2024-04-21 14:04:00 -0300

    ajusta forma de envio de arquivo para servidor ftp

--- src/src/FTPClientApplication.java
+++ src/src/FTPClientApplication.java
@@ -38,38 +38,6 @@ public class FTPClientApplication {
 
 
             send(socket, writer, reader);
-            System.exit(0);
-//            File file = new File(filePath);
-//            long fileSize = file.length();
-//            writer.write("PASV\r\n");
-//            writer.flush();
-//            response = reader.readLine();
-//            //pegar esse valor para passar junto do PORT
-//            System.out.println("Response: " + response);
-
-            //PRECISO MANDAR DE QUAL PORTA ESTOU FALANDO 127.0.0.1:porta
-
-//            String[] parts = response.split(",");
-//            String ip = parts[0].substring(parts[0].lastIndexOf("(") + 1) + "." + parts[1] + "." + parts[2] + "." + parts[3];
-//            int portData = Integer.parseInt(parts[4]) * 256 + Integer.parseInt(parts[5].substring(0, parts[5].indexOf(")")));
-//            System.out.println("Data Connection IP: " + ip);
-//            System.out.println("Data Connection Port: " + portData);
-//
-//            try (Socket dataSocket = new Socket(ip, portData);
-//                 BufferedOutputStream dataOutput = new BufferedOutputStream(dataSocket.getOutputStream());
-//                 FileInputStream fileInput = new FileInputStream(file)) {
-//
-//                byte[] buffer = new byte[1024];
-//                int bytesRead;
-//                while ((bytesRead = fileInput.read(buffer)) != -1) {
-//                    dataOutput.write(buffer, 0, bytesRead);
-//                }
-//            }
-//
-//            writer.write("STOR " + remoteDirPath + "/" + file.getName() + "\r\n");
-//            writer.flush();
-//            response = reader.readLine();
-//            System.out.println("Response: " + response);
             System.out.println(reader.readLine());
 
         } catch (IOException e) {
@@ -80,6 +48,7 @@ public class FTPClientApplication {
     private static void send(Socket socket, BufferedWriter writer, BufferedReader reader) throws IOException {
         FileInputStream fis = new FileInputStream("./files/origin.txt");
         BufferedInputStream bis = new BufferedInputStream(fis);
+        OutputStream outputStream = socket.getOutputStream();
 
         writer.write("STOR server.txt\r\n");
         writer.flush();
@@ -87,13 +56,10 @@ public class FTPClientApplication {
         byte[] buffer = new byte[1024];
         int bytesRead;
         while ((bytesRead = bis.read(buffer)) != -1) {
-            socket.getOutputStream().write(buffer, 0, bytesRead);
+            outputStream.write(buffer, 0, bytesRead);
         }
 
-        String response = reader.readLine();
-        System.out.println(response);
-
-        bis.close();
         fis.close();
+        bis.close();
     }
 }
